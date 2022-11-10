8a0ea91c9ebe9b88102cbb2139cf1f3e063af298
Quang Huy <quanghuy3014@gmail.com>
2022-11-10 18:09:26 +0700

    Fix social link

--- src/components/home/HeaderSocial.jsx
+++ src/components/home/HeaderSocial.jsx
@@ -2,14 +2,26 @@ import React from "react";
 function HeaderSocials() {
   return (
     <div className="home__socials">
-      <a href="" className="home__social-link" target="blank">
-        <i className="fa-brands fa-instagram"></i>
+      <a
+        href="https://www.linkedin.com/in/quanghuy14/"
+        className="home__social-link"
+        target="blank"
+      >
+        <i class="fa fa-linkedin" aria-hidden="true"></i>
       </a>
-      <a href="" className="home__social-link" target="blank">
-        <i className="fa-brands fa-twitter"></i>
-      </a>
-      <a href="" className="home__social-link" target="blank">
+      <a
+        href="https://www.facebook.com/nqh148/"
+        className="home__social-link"
+        target="blank"
+      >
         <i className="fa-brands fa-facebook"></i>
+      </a>
+      <a
+        href="https://github.com/qh14"
+        className="home__social-link"
+        target="blank"
+      >
+        <i class="fa-brands fa-github"></i>
       </a>
     </div>
   );
